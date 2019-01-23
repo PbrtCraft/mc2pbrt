afe16b0b3f85b093da70907745d40300d59831a5
Mudream <mudream4869@gmail.com>
2019-01-23 22:05:58 +0800

    water heightfield

--- pbrtwriter.py
+++ pbrtwriter.py
@@ -45,29 +45,110 @@ class WaterSolver:
         if z < 0 or z >= self.Z : return None
         return self.level[y][z][x]
 
+    def getHeight(self, pt):
+        l = self.getLevel(pt)
+        if l == None: return None
+        return self._level2height(l)
+
+    def calAvg(self, xs):
+        cnt = 0
+        sum_xs = 0
+        for x in xs:
+            cnt += 1
+            sum_xs += x or 0
+        return sum_xs/cnt
+
     def render(self, fout):
         print("Writing water blocks...")
-        fout.write('Material "glass"\n')
+        fout.write('Material "glass" "float eta" [1.33] "rgb Kt" [.28 .72 1]\n')
         for x,y,z in tqdm([(x, y, z) for x in range(self.X) for y in range(self.Y) for z in range(self.Z)],
                           ascii=True):
             if self.level[y][z][x] == None: continue
             pt = (x, y, z)
             h = self._level2height(self.level[y][z][x])
             fout.write('AttributeBegin\n')
-            fout.write('Translate %f %f %f\n' % plus(pt, (.5, h/2, .5)))
-            cube = (1, h/2, 1)
-            for facename in pt_map:
-                cull_pt = plus_i(cullface_map[facename], pt)
-                if self.getLevel(cull_pt) != None:
-                    continue
-                delta_f, l_f, dir_, shape = pt_map[facename]
-                delta = delta_f(cube)
-                l1, l2 = l_f(cube)
+            fout.write('Translate %f %f %f\n' % plus(pt, (.5, 0, .5)))
+            #cube = (1, h/2, 1)
+
+            if self.getLevel((x, y+1, z)) != None:
+                # When a water block is on current block, the water should be full.
+                ps = [1]*9
+            else:
+                dt = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+                hs = [None]*9
+                for i in range(9):
+                    hs[i] = self.getHeight((x+dt[i][1], y, z+dt[i][0]))
+
+                ps = [[0, 1, 3, 4], [1, 4], [1, 2, 4, 5],
+                      [3, 4],       [4],    [4, 5],
+                      [3, 4, 6, 7], [4, 7], [4, 5, 7, 8]]
+
+                ps = [self.calAvg(map(lambda x:hs[x], p)) for p in ps]
+
+            if self.getLevel((x, y+1, z)) == None:
+                from math import pi
                 fout.write('AttributeBegin\n')
-                fout.write('  Translate %f %f %f\n' % delta)
-                fout.write('  Shape "%s" "float l1" [%f] "float l2" [%f] ' % (shape, l1, l2) + 
-                           '  "float dir" [%d]' % dir_)
+                fout.write('  Translate -.5 0 -.5\n')
+                fout.write('  Shape "heightfield" "integer nv" [3] "integer nu" [3]\n' + 
+                           '  "float Py" [%f %f %f %f %f %f %f %f %f]' % tuple(ps))
                 fout.write('AttributeEnd\n')
+
+            if self.getLevel((x, y-1, z)) == None:
+                fout.write('Shape "quady"\n')
+
+            def param(pts):
+                flat_pts = []
+                for pt in pts:
+                    flat_pts += list(pt)
+                return "[" + " ".join(map(str, flat_pts)) + "]"
+
+            def trimesh(mv, pts, inds):
+                fout.write('AttributeBegin\n')
+                fout.write('  Translate %f %f %f\n' % mv)
+                fout.write('  Shape "trianglemesh" "point P" %s ' % param(pts) +
+                           '  "integer indices" %s' % param(inds))
+                fout.write('AttributeEnd\n')
+            
+            def rev(inds):
+                ret = list(inds)
+                for i in range(len(ret)):
+                    tri = ret[i]
+                    ret[i] = (tri[0], tri[2], tri[1])
+                return ret
+
+            indx = [(0, 1, 2), (1, 3, 2), (2, 3, 4), (3, 5, 4)] 
+            if self.getLevel((x-1, y, z)) == None:
+                pts = [
+                    (0, 0, -.5), (0, ps[0], -.5),
+                    (0, 0, 0),   (0, ps[3], 0),
+                    (0, 0, .5),  (0, ps[6], .5),
+                ]
+                trimesh((-.5, 0, 0), pts, rev(indx))
+            
+            if self.getLevel((x+1, y, z)) == None:
+                pts = [
+                    (0, 0, -.5), (0, ps[2], -.5),
+                    (0, 0, 0),   (0, ps[5], 0),
+                    (0, 0, .5),  (0, ps[8], .5),
+                ]
+                trimesh((.5, 0, 0), pts, indx)
+
+            if self.getLevel((x, y, z-1)) == None:
+                pts = [
+                    (-.5, 0, 0), (-.5, ps[0], 0),
+                    (0, 0, 0),   (0, ps[1], 0),
+                    (.5, 0, 0),  (.5, ps[2], 0),
+                ]
+                trimesh((0, 0, -.5), pts, indx)
+
+            if self.getLevel((x, y, z+1)) == None:
+                pts = [
+                    (-.5, 0, 0), (-.5, ps[6], 0),
+                    (0, 0, 0),   (0, ps[7], 0),
+                    (.5, 0, 0),  (.5, ps[8], 0),
+                ]
+                trimesh((0, 0, .5), pts, rev(indx))
+
             fout.write('AttributeEnd\n')
 
 
